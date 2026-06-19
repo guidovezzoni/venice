b6f0eff3fc5ccbf7866b4b3dec1618004dfda658
guido <guido.vezzoni.development@gmail.com>
2026-06-19 20:32:03 +0200

    kover added

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.ksp) apply false
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.detekt) apply false
+    alias(libs.plugins.kover) apply false
 }
